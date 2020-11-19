690a3d76982821b8bfc81657b709ee2b6cdd7032
Raindy Hardiansyah <raindyhardian@gmail.com>
2020-11-19 09:31:52 +0700

    update navbar for mobile display

--- src/features/Navbar/Navbar.js
+++ src/features/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./navbar.css";
 import { NavLink } from "react-router-dom";
 
@@ -17,6 +17,26 @@ const pages = [
   },
 ];
 const Navbar = () => {
+  const navListRef = useRef();
+  let expanded = false;
+  const navbarToggle = () => {
+    const list = navListRef.current.children;
+    // console.log(list[0].classList);
+    if (!expanded) {
+      navListRef.current.classList.remove("close");
+      for (let item of list) {
+        item.classList.remove("hideText");
+      }
+      navListRef.current.classList.add("open");
+    } else {
+      navListRef.current.classList.remove("open");
+      for (let item of list) {
+        item.classList.add("hideText");
+      }
+      navListRef.current.classList.add("close");
+    }
+    expanded = !expanded;
+  };
   return (
     <div className="navbar__container">
       <NavLink to="/" className="navbar__link">
@@ -25,7 +45,10 @@ const Navbar = () => {
           <div className="navbar__name">Poke-ref</div>
         </div>
       </NavLink>
-      <div className="navbar__list">
+      <div className="navbar__expand" onClick={navbarToggle}>
+        Expand
+      </div>
+      <div className="navbar__list" ref={navListRef}>
         {pages.map(({ label, route }) => (
           <NavLink
             exact
