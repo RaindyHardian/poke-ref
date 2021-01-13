4c8ffdfdda59d9db21d5ec0577e3211379a5ee30
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-01-13 20:28:24 +0700

    update background and css

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../../features/Navbar/Navbar";
+import "./page.css"
 
 const Page = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <main>{children}</main>
+      <main className="page__main">{children}</main>
     </div>
   );
 };
